60c39f6a23d5f277973d5261f83f5186266c57db
Rodrigo <160552648+RodrigoB093@users.noreply.github.com>
2025-01-16 20:10:26 -0500

    Create animales.py

---
+++ animales.py
@@ -0,0 +1,18 @@
+import requests
+import random
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
